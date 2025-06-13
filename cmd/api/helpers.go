fd30780e0c3eb1967a305dd40f6e0bec16a00e37
Macsen Casaus <macsencasaus@gmail.com>
2025-06-13 12:34:13 -0500

    refactor cmd to reduce files

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
+	"path/filepath"
+	"regexp"
 	"runtime/debug"
+	"strconv"
 
 	"github.com/macsencasaus/jetapi/internal/sites"
 )
@@ -46,4 +51,89 @@ func (app *application) render(
 	if err != nil {
 		app.serverError(w, err)
 	}
+}
+
+func (app *application) parseAPIQueries(
+	w http.ResponseWriter,
+	r *http.Request,
+) (*sites.APIQueries, error) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		return nil, fmt.Errorf("%d", http.StatusMethodNotAllowed)
+	}
+	queryParams := r.URL.Query()
+
+	reg := queryParams.Get("reg")
+	if reg == "" {
+		return nil, fmt.Errorf("%d", http.StatusNotFound)
+	}
+
+	// is alphanumeric and may include '-'
+	isAlpha := regexp.MustCompile(`^[a-zA-Z0-9-]+$`).MatchString(reg)
+	if !isAlpha {
+		return nil, fmt.Errorf("%d", http.StatusBadRequest)
+	}
+
+	photos, err := handleNumQuery(queryParams, "photos")
+	if err != nil {
+		return nil, err
+	}
+	if photos == -1 {
+		photos = 3
+	}
+
+	flights, err := handleNumQuery(queryParams, "flights")
+	if err != nil {
+		return nil, err
+	}
+	if flights == -1 {
+		flights = 20
+	}
+
+	q := &sites.APIQueries{Reg: reg, Photos: photos, Flights: flights}
+	return q, nil
+}
+
+func handleNumQuery(qp url.Values, query string) (int, error) {
+	resStr := qp.Get(query)
+	if resStr == "" {
+		return -1, nil
+	}
+	res, err := strconv.Atoi(resStr)
+	if err != nil || res < 0 {
+		return 0, fmt.Errorf("%d", http.StatusBadRequest)
+	}
+	return res, nil
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partial/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/macsencasaus/jetapi/internal/sites"
+)
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
@@ -15,4 +21,73 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/querybuilder", app.queryBuilder)
 
 	return mux
+}
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+	page := "home.tmpl"
+	app.render(w, http.StatusOK, page, nil)
+}
+
+func (app *application) api(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	q, err := app.parseAPIQueries(w, r)
+	if err != nil {
+		app.badRequest(w)
+		return
+	}
+
+	sr, err := sites.Scrape(q)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	jsonResult, err := json.Marshal(sr)
+	if err != nil {
+		app.serverError(w, fmt.Errorf("Error encoding json: %v", err))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResult)
+}
+
+func (app *application) aircraftSearch(w http.ResponseWriter, r *http.Request) {
+	page := "aircraft.tmpl"
+	q, err := app.parseAPIQueries(w, r)
+	if err != nil {
+		app.notFoundPage(w)
+		return
+	}
+	q = &sites.APIQueries{Reg: q.Reg, Photos: 3, Flights: 8}
+	ji, err := sites.Scrape(q)
+	if err != nil {
+		app.notFoundPage(w)
+		return
+	}
+	app.render(w, http.StatusOK, page, ji)
+}
+
+func (app *application) documentation(w http.ResponseWriter, r *http.Request) {
+	page := "documentation.tmpl"
+	app.render(w, http.StatusOK, page, nil)
+}
+
+func (app *application) queryBuilder(w http.ResponseWriter, r *http.Request) {
+	page := "querybuilder.tmpl"
+	app.render(w, http.StatusOK, page, nil)
+}
+
+func (app *application) notFoundPage(w http.ResponseWriter) {
+	page := "notfound.tmpl"
+	app.render(w, http.StatusNotFound, page, nil)
 }
